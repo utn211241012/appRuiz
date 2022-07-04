939fbef8cceadc40a72902e84ee7a67bfdb66c62
utn211241012 <101811558+utn211241012@users.noreply.github.com>
2022-07-03 22:37:27 -0500

    Update HomeController.java

--- src/main/java/com/Ferreira/Huerta/Luis/Angel/org/Controller/HomeController.java
+++ src/main/java/com/Ferreira/Huerta/Luis/Angel/org/Controller/HomeController.java
@@ -1,5 +1,7 @@
 package com.Ferreira.Huerta.Luis.Angel.org.Controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -24,6 +26,6 @@ public class HomeController {
 		List<Categoria> catego = CategoriasServceImp.ObtenerTodas();
 		model.addAttribute("vacantes", lista);
 		model.addAttribute("categorias", catego);
-		return "/home";
+		return "home";
 	}
 }
